29105a121eaa2cc3f6925c66332f7a572bcb0bc7
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-31 13:33:14 +0100

    Fix linters

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const objectlist = [
-=======
 let objectlist =[
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
   {
     bookname: 'Java books',
     author: 'Ali',
@@ -12,16 +8,6 @@ let objectlist =[
     author: 'Ahmad',
   },
   {
-<<<<<<< HEAD
-    bookname: 'HTML Books',
-    author: 'Ali',
-  },
-];
-
-function generateBooks({
-  author, bookname,
-}) {
-=======
     bookname:'HTML Books',
     author:'Ali'
   }
@@ -30,7 +16,6 @@ function generateBooks({
 function generateBooks({
   bookname, author
 }){
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
   return `
   <li>
   <span class="name">${bookname}</span><br>
@@ -40,15 +25,6 @@ function generateBooks({
   `;
 }
 
-<<<<<<< HEAD
-const lists = document.querySelector('#book-list ul');
-const btnAdd = document.querySelector('.add');
-
-// delete books
-
-lists.addEventListener('click', (e) => {
-  if (e.target.className === 'delete') {
-=======
 const bookContainer=document.querySelector('.book-container ul');
 
 const bookList=objectlist.map(book => generateBooks(book)).join('');
@@ -66,7 +42,6 @@ const lists =document.querySelector ('#book-list ul');
 bookContainer.addEventListener ('click' ,function (e) {
   const title = e.target.parentElement.firstElementChild.textContent;
   if (e.target.className =='delete') {
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
     const li = e.target.parentElement;
     bookContainer.removeChild(li);
     objectlist = objectlist.filter((obj) => obj.bookname !== title);
@@ -78,42 +53,19 @@ bookContainer.addEventListener ('click' ,function (e) {
 
 // add books
 
-<<<<<<< HEAD
-const addForm = document.forms['add-books'];
-=======
 const addForm =document.forms['add-books'];
 const form =document.querySelector('form');
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
 
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const titleName = addForm.querySelector('.name').value;
   const authorName = addForm.querySelector('.author').value;
-<<<<<<< HEAD
-
-  // local storage
-
-  // titleName.addEventListener('input' , letter =>{
-  //   titleName.textContent=letter.target.value;
-  //   authorName.textContent=letter.target.value;
-  // })
-
-  // const saveToLocalStorage =() =>{
-  //   localStorage.setItem('inputName',titleName.textContent);
-  //   localStorage.setItem('inputAuthor',authorName.textContent);
-  // }
-
-  // btnAdd.addEventListener('click', saveToLocalStorage);
-
-  // create element
-=======
   objectlist.push({
     bookname:titleName , author:authorName
   })
   form.reset();
   localStorage.setItem('books',JSON.stringify(objectlist))
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
 
   const li = document.createElement('li');
   const title = document.createElement('span');
@@ -138,12 +90,6 @@ addForm.addEventListener('submit', (e) => {
 
   // content
 
-<<<<<<< HEAD
-  title.textContent = titleName;
-  author.textContent = authorName;
-  deleteBtn.textContent = 'delete';
-});
-=======
 //append
 
 li.appendChild(title);
@@ -163,4 +109,3 @@ deleteBtn.textContent='delete';
 
 })
 
->>>>>>> df7048d8f413f448c740094d22849c8a539a643d
